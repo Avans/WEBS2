854a8a957f9d33ade5da0cf044ee5b33caa75a28
Rik Meijer <ha.meijer@avans.nl>
2018-12-19 15:11:41 +0100

    add include for functions

--- Week 1/Kalender/calendar.php
+++ Week 1/Kalender/calendar.php
@@ -8,6 +8,9 @@
 </head>
 
 <?php
+
+require __DIR__ . 'functions.php';
+
 const dayStrings = array("Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun");
 
 $nr_of_days_in_month = date("t");
@@ -61,72 +64,6 @@ while ($count < $nr_of_entries) {
 
 create_calendar_end();
 // And we're done.
-
-// *******************************************
-// ************** Functions ******************
-// *******************************************
-function create_header()
-{
-    create_row_start();
-
-    foreach (dayStrings as $dayName) {
-        echo("<th>");
-        echo($dayName);
-        echo("</th>");
-    }
-
-    create_row_end();
-}
-
-function create_row_start()
-{
-    echo("<tr>");
-}
-
-function create_row_end()
-{
-    echo("</tr>");
-}
-
-function create_month_title()
-{
-    echo("<tr><td colspan='7' id='calendar_month'>" . date("F Y") . "</td></tr>");
-}
-
-function create_entry($day_number, $day_in_week = 0)
-{
-    switch ($day_in_week) {
-        case 5:
-            echo("<td class = 'calendar_entry saturday_entry'>");
-            break;
-
-        case 6:
-            echo("<td class = 'calendar_entry sunday_entry'>");
-            break;
-
-        default:
-            echo("<td class = 'calendar_entry'>");
-            break;
-    }
-
-    if ($day_number > 0) {
-        echo("<div class = 'entry_date'>$day_number</div>");
-        echo("<div class = 'entry_line'></div>");
-        echo("<div class = 'entry_line'></div>");
-        echo("<div class = 'entry_line'></div>");
-    }
-    echo("</td>");
-}
-
-function create_calendar_start()
-{
-    echo "<table class = 'calendar'>";
-}
-
-function create_calendar_end()
-{
-    echo "</table>";
-}
 
 ?>
 </html>
