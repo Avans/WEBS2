891bb303a01047848fd3aa36fc3a3e97b30379aa
Rik Meijer <ha.meijer@avans.nl>
2019-01-10 10:06:51 +0100

    delete unused functions.php

--- Week 2/KalenderOO/Calendar.php
+++ Week 2/KalenderOO/Calendar.php
@@ -2,8 +2,6 @@
 
 namespace Calendar;
 
-require __DIR__ . DIRECTORY_SEPARATOR . 'functions.php';
-
 class Calendar
 {
     const dayStrings = array("Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun");
