1f4ed3911108f55c7c909d73d40d706efc941375
Rik Meijer <ha.meijer@avans.nl>
2019-01-10 10:24:27 +0100

    fix unnecessary spaces in HTML

--- Week 2/KalenderOO/HTMLCalendar.php
+++ Week 2/KalenderOO/HTMLCalendar.php
@@ -1,13 +1,10 @@
 <?php
 
-
 namespace Calendar;
 
 
 class HTMLCalendar
 {
-
-
     public function renderRowStart()
     {
         echo("<tr>");
@@ -23,30 +20,30 @@ class HTMLCalendar
     {
         switch ($day_in_week) {
             case 5:
-                echo("<td class = 'calendar_entry saturday_entry'>");
+                echo("<td class='calendar_entry saturday_entry'>");
                 break;
 
             case 6:
-                echo("<td class = 'calendar_entry sunday_entry'>");
+                echo("<td class='calendar_entry sunday_entry'>");
                 break;
 
             default:
-                echo("<td class = 'calendar_entry'>");
+                echo("<td class='calendar_entry'>");
                 break;
         }
 
         if ($day_number > 0) {
-            echo("<div class = 'entry_date'>$day_number</div>");
-            echo("<div class = 'entry_line'></div>");
-            echo("<div class = 'entry_line'></div>");
-            echo("<div class = 'entry_line'></div>");
+            echo("<div class='entry_date'>$day_number</div>");
+            echo("<div class='entry_line'></div>");
+            echo("<div class='entry_line'></div>");
+            echo("<div class='entry_line'></div>");
         }
         echo("</td>");
     }
 
     public function renderStart($days, $monthTitle)
     {
-        echo "<table class = 'calendar'>";
+        echo "<table class='calendar'>";
         $this->renderMonthName(count($days), $monthTitle);
         $this->renderDays($days);
     }
