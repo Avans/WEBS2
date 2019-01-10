0884c5b8fee3dab008b558383fcbe64c1183b14c
Rik Meijer <ha.meijer@avans.nl>
2019-01-10 10:01:20 +0100

    move all rendering to render method

--- Week 2/KalenderOO/Calendar.php
+++ Week 2/KalenderOO/Calendar.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Calendar;
 
 require __DIR__ . DIRECTORY_SEPARATOR . 'functions.php';
@@ -6,5 +7,62 @@ require __DIR__ . DIRECTORY_SEPARATOR . 'functions.php';
 class Calendar
 {
     const dayStrings = array("Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun");
+
+    public function render()
+    {
+
+
+        $nr_of_days_in_month = date("t");
+        $current_day_of_month = date("j");
+        $current_day_of_week = date("w"); // 0 = Sunday, 6 = Saturday
+
+        $offset_at_start = $current_day_of_week - ($current_day_of_month % 7);
+        $nr_of_entries = 0;
+
+        // calculate the offset for the first date line
+        if ($offset_at_start < 0) {
+            $offset_at_start = 7 + $offset_at_start;
+        } else {
+            $offset_at_start = 7 - $offset_at_start;
+        }
+
+        // calculate the number of calendar entries needed for this month
+        // including the empty ones needed before and after the actual days
+        $nr_of_entries = 7 * round(($offset_at_start + $nr_of_days_in_month) / 7);
+
+        // Create the calendar
+        create_calendar_start();
+        // Start with month indication
+        create_month_title();
+        // Start filling the table with a header
+        create_header();
+        // Now do the days, both empty and with date
+        create_row_start();
+        $count = 0;
+        $day_count = 1;
+        while ($count < $nr_of_entries) {
+            if ($offset_at_start > 0) {
+                create_entry(0);
+                $offset_at_start--;
+            } else {
+                if ($day_count <= $nr_of_days_in_month) {
+                    create_entry($day_count, $count % 7);
+                    $day_count++;
+                } else {
+                    create_entry(0);
+                }
+            }
+
+            $count++;
+
+            if ($count % 7 === 0) {
+                create_row_start();
+                create_row_end();
+            }
+        }
+
+        create_calendar_end();
+        // And we're done.
+    }
 
 }
--- Week 2/KalenderOO/calendar.php
+++ Week 2/KalenderOO/calendar.php
@@ -11,58 +11,8 @@
 
 require "Calendar.php";
 
-
-$nr_of_days_in_month = date("t");
-$current_day_of_month = date("j");
-$current_day_of_week = date("w"); // 0 = Sunday, 6 = Saturday
-
-$offset_at_start = $current_day_of_week - ($current_day_of_month % 7);
-$nr_of_entries = 0;
-
-// calculate the offset for the first date line
-if ($offset_at_start < 0) {
-    $offset_at_start = 7 + $offset_at_start;
-} else {
-    $offset_at_start = 7 - $offset_at_start;
-}
-
-// calculate the number of calendar entries needed for this month
-// including the empty ones needed before and after the actual days
-$nr_of_entries = 7 * round(($offset_at_start + $nr_of_days_in_month) / 7);
-
-// Create the calendar
-Calendar\create_calendar_start();
-// Start with month indication
-Calendar\create_month_title();
-// Start filling the table with a header
-Calendar\create_header();
-// Now do the days, both empty and with date
-Calendar\create_row_start();
-$count = 0;
-$day_count = 1;
-while ($count < $nr_of_entries) {
-    if ($offset_at_start > 0) {
-        Calendar\create_entry(0);
-        $offset_at_start--;
-    } else {
-        if ($day_count <= $nr_of_days_in_month) {
-            Calendar\create_entry($day_count, $count % 7);
-            $day_count++;
-        } else {
-            Calendar\create_entry(0);
-        }
-    }
-
-    $count++;
-
-    if ($count % 7 === 0) {
-        Calendar\create_row_start();
-        Calendar\create_row_end();
-    }
-}
-
-Calendar\create_calendar_end();
-// And we're done.
+$calendar = new \Calendar\Calendar();
+$calendar->render();
 
 ?>
 </html>
