0a41face5cfde2963045b55aaf2b09ad725bbd64
Rik Meijer <ha.meijer@avans.nl>
2019-01-10 12:04:54 +0100

    month should be instantiated just before its used

--- Week 2/KalenderOO/calendar.php
+++ Week 2/KalenderOO/calendar.php
@@ -12,8 +12,8 @@
 require __DIR__ . DIRECTORY_SEPARATOR . "HTMLCalendar.php";
 require __DIR__ . DIRECTORY_SEPARATOR . 'Month.php';
 
-$month = new \Calendar\Month(date("t"), date("j"), date("w"));
 $calendar = new \Calendar\HTMLCalendar(array("Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"));
+$month = new \Calendar\Month(date("t"), date("j"), date("w"));
 $calendar->renderMonth(date("F Y"), $month);
 
 ?>
