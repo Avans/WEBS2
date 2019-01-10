e309cf5844ef05c4dac97fc630692cbc32fda5d2
Rik Meijer <ha.meijer@avans.nl>
2019-01-10 09:59:45 +0100

    introduce Calendar class and move all functions within same namespace

---
+++ Week 2/KalenderOO/Calendar.php
@@ -0,0 +1,10 @@
+<?php
+namespace Calendar;
+
+require __DIR__ . DIRECTORY_SEPARATOR . 'functions.php';
+
+class Calendar
+{
+    const dayStrings = array("Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun");
+
+}
---
+++ Week 2/KalenderOO/functions.php
@@ -0,0 +1,68 @@
+<?php
+namespace Calendar;
+
+// *******************************************
+// ************** Functions ******************
+// *******************************************
+function create_header()
+{
+    create_row_start();
+
+    foreach (Calendar::dayStrings as $dayName) {
+        echo("<th>");
+        echo($dayName);
+        echo("</th>");
+    }
+
+    create_row_end();
+}
+
+function create_row_start()
+{
+    echo("<tr>");
+}
+
+function create_row_end()
+{
+    echo("</tr>");
+}
+
+function create_month_title()
+{
+    echo("<tr><td colspan='7' id='calendar_month'>" . date("F Y") . "</td></tr>");
+}
+
+function create_entry($day_number, $day_in_week = 0)
+{
+    switch ($day_in_week) {
+        case 5:
+            echo("<td class = 'calendar_entry saturday_entry'>");
+            break;
+
+        case 6:
+            echo("<td class = 'calendar_entry sunday_entry'>");
+            break;
+
+        default:
+            echo("<td class = 'calendar_entry'>");
+            break;
+    }
+
+    if ($day_number > 0) {
+        echo("<div class = 'entry_date'>$day_number</div>");
+        echo("<div class = 'entry_line'></div>");
+        echo("<div class = 'entry_line'></div>");
+        echo("<div class = 'entry_line'></div>");
+    }
+    echo("</td>");
+}
+
+function create_calendar_start()
+{
+    echo "<table class = 'calendar'>";
+}
+
+function create_calendar_end()
+{
+    echo "</table>";
+}
