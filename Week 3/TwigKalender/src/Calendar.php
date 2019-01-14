0f4c73df044bcf32540af528e2e03d6dc709c7fe
Rik Meijer <ha.meijer@avans.nl>
2019-01-14 15:59:07 +0100

    refactored so all HTML is using twig

--- Week 3/TwigKalender/src/Calendar.php
+++ Week 3/TwigKalender/src/Calendar.php
@@ -7,27 +7,13 @@ require __DIR__ . DIRECTORY_SEPARATOR . 'Month.php';
 
 class Calendar
 {
-    private $date;
+    private $month;
     private $days;
-    private $calenderEntryWeekdayClasses;
-    public $widthRenderedCalendarInDays;
 
     public function __construct(\DateTime $date, $days)
     {
         $this->month = new \Calendar\Month($date);
-
         $this->days = $days;
-        $this->widthRenderedCalendarInDays = count($days);
-        $this->calenderEntryWeekdayClasses = array_map(function($dayName){
-            switch ($dayName) {
-                case "Sat":
-                    return "saturday_entry";
-                case "Sun":
-                    return "sunday_entry";
-                default:
-                    return "";
-            }
-        }, $days);
     }
 
     public function shiftMonth()
@@ -40,52 +26,7 @@ class Calendar
         return $this->month->formatLabel();
     }
 
-    private function renderRowStart()
-    {
-        echo("<tr>");
-    }
-
-    private function renderRowEnd()
-    {
-        echo("</tr>");
-    }
-
-    public function calculateOffsetAtStart() {
-        return $this->month->calculateOffsetAtStart($this->days);
-    }
-
-    public function renderDates()
-    {
-        $day_count = 1;
-        for ($count = $this->calculateOffsetAtStart(); $count < ($this->widthRenderedCalendarInDays * $this->month->calculateNumberOfWeeks($this->days));) {
-            if ($this->month->beyondEndOfMonth($day_count)) {
-                $this->renderEmptyDay();
-            } else {
-                $this->renderDay($day_count, $count % $this->widthRenderedCalendarInDays);
-                $day_count++;
-            }
-
-            $count++;
-
-            if ($count % $this->widthRenderedCalendarInDays === 0) {
-                $this->renderRowEnd();
-                $this->renderRowStart();
-            }
-        }
-    }
-
-    private function renderEmptyDay()
-    {
-        echo "<td class='calendar_entry'></td>";
-    }
-
-    private function renderDay($monthDay, $weekDay)
-    {
-        echo("<td class='calendar_entry " . $this->calenderEntryWeekdayClasses[$weekDay] . "'>");
-        echo("<div class='entry_date'>$monthDay</div>");
-        echo("<div class='entry_line'></div>");
-        echo("<div class='entry_line'></div>");
-        echo("<div class='entry_line'></div>");
-        echo("</td>");
+    public function dates() {
+        return new Dates($this->month, $this->days);
     }
 }
---
+++ Week 3/TwigKalender/src/Dates.php
@@ -0,0 +1,92 @@
+<?php
+namespace Calendar;
+
+class Dates implements \Iterator {
+
+    private $month;
+    private $days;
+
+    private $offsetAtStart;
+
+    private $currentEntry;
+    private $totalEntries;
+
+    public function __construct(Month $month, $days)
+    {
+        $this->month = $month;
+        $this->days = $days;
+
+        $this->offsetAtStart = $month->calculateOffsetAtStart($days);
+        $this->rewind();
+        $this->totalEntries = (count($days) * $month->calculateNumberOfWeeks($days));
+    }
+
+    /**
+     * Return the current element
+     * @link https://php.net/manual/en/iterator.current.php
+     * @return mixed Can return any type.
+     * @since 5.0.0
+     */
+    public function current()
+    {
+        $day_count = $this->currentEntry - $this->offsetAtStart + 1;
+        if ($this->currentEntry < $this->offsetAtStart) {
+            return 'empty';
+        } elseif ($this->month->beyondEndOfMonth($day_count)) {
+            return 'empty';
+        } else {
+            return [
+                'weekday' => $this->currentEntry % count($this->days),
+                'weekday_class' => call_user_func(function($dayName){
+                    switch ($dayName) {
+                        case "Sat":
+                            return "saturday_entry";
+                        case "Sun":
+                            return "sunday_entry";
+                        default:
+                            return "";
+                    }
+                }, $this->days[$this->currentEntry % count($this->days)]),
+                'monthday' =>  $day_count,
+            ];
+        }
+    }
+
+    /**
+     * Move forward to next element
+     * @link https://php.net/manual/en/iterator.next.php
+     * @return void Any returned value is ignored.
+     * @since 5.0.0
+     */
+    public function next()
+    {
+        $this->currentEntry++;
+    }
+
+    /**
+     * Return the key of the current element
+     * @link https://php.net/manual/en/iterator.key.php
+     * @return mixed scalar on success, or null on failure.
+     * @since 5.0.0
+     */
+    public function key()
+    {
+        return $this->currentEntry;
+    }
+
+    public function valid()
+    {
+        return $this->currentEntry < $this->totalEntries;
+    }
+
+    /**
+     * Rewind the Iterator to the first element
+     * @link https://php.net/manual/en/iterator.rewind.php
+     * @return void Any returned value is ignored.
+     * @since 5.0.0
+     */
+    public function rewind()
+    {
+        $this->currentEntry = 0;
+    }
+}
