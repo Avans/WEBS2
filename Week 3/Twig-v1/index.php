4d1a6a3d359264d917065595d4313db118dfaf74
Rik Meijer <ha.meijer@avans.nl>
2019-01-14 12:12:16 +0100

    add Twig example

---
+++ Week 3/Twig-v1/index.php
@@ -0,0 +1,7 @@
+<?php
+require_once __DIR__ . '/vendor/autoload.php';
+
+$loader = new Twig_Loader_Filesystem(__DIR__ . DIRECTORY_SEPARATOR . 'views');
+$twig = new Twig_Environment($loader);
+
+echo $twig->render('index.html', ['name' => 'World']);
