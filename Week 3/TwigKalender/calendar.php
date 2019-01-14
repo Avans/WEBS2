84466ecc63a635c0d56410d157e180e8d2065bf2
Rik Meijer <ha.meijer@avans.nl>
2019-01-14 13:15:52 +0100

    init twig and use composer autoload to load Calendar classes

---
+++ Week 3/TwigKalender/calendar.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<!-- Author: Jasper van Rosmalen, Rik Meijer -->
+<!-- Avans University of Applied Technology -->
+
+<head>
+    <title>Kalender Opdracht</title>
+    <link rel="stylesheet" type="text/css" href="calendar_style.css">
+</head>
+
+<?php
+require __DIR__ . DIRECTORY_SEPARATOR . "vendor/autoload.php";
+$calendar = new \Calendar\Calendar(["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]);
+
+try {
+    $month = new \Calendar\Month(new \DateTime(date("Y-1-1")));
+} catch (\Exception $exception) {
+    print $exception->getTraceAsString();
+    exit;
+}
+for ($month_count = 1; $month_count <= 12; $month_count++) {
+    $calendar->renderMonth($month);
+    $month = $month->next();
+}
+?>
+</html>
---
+++ Week 3/TwigKalender/src/Calendar.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace Calendar;
+
+require __DIR__ . DIRECTORY_SEPARATOR . 'Month.php';
+
+
+class Calendar
+{
+    private $days;
+    private $calenderEntryWeekdayClasses;
+    private $widthRenderedCalendarInDays;
+
+    public function __construct($days)
+    {
+        $this->days = $days;
+        $this->widthRenderedCalendarInDays = count($days);
+        $this->calenderEntryWeekdayClasses = array_map(function($dayName){
+            switch ($dayName) {
+                case "Sat":
+                    return "saturday_entry";
+                case "Sun":
+                    return "sunday_entry";
+                default:
+                    return "";
+            }
+        }, $days);
+    }
+
+    public function renderMonth(Month $month)
+    {
+        echo "<table class='calendar'>";
+        $this->renderMonthName($month);
+        $this->renderWeekDays();
+        $this->renderDates($month);
+        echo "</table>";
+    }
+
+    private function renderMonthName(Month $month)
+    {
+        echo("<tr><td colspan='" . $this->widthRenderedCalendarInDays . "' id='calendar_month'>" . $month->formatLabel() . "</td></tr>");
+    }
+
+    private function renderWeekDays()
+    {
+        $this->renderRowStart();
+
+        foreach ($this->days as $day) {
+            echo("<th>");
+            echo($day);
+            echo("</th>");
+        }
+
+        $this->renderRowEnd();
+    }
+
+    private function renderRowStart()
+    {
+        echo("<tr>");
+    }
+
+    private function renderRowEnd()
+    {
+        echo("</tr>");
+    }
+
+    private function renderDates(Month $month)
+    {
+        $this->renderRowStart();
+
+        for ($count = 0; $count < $month->calculateOffsetAtStart($this->days); $count++) {
+            $this->renderEmptyDay();
+        }
+
+        $day_count = 1;
+        while ($count < ($this->widthRenderedCalendarInDays * $month->calculateNumberOfWeeks($this->days))) {
+            if ($month->beyondEndOfMonth($day_count)) {
+                $this->renderEmptyDay();
+            } else {
+                $this->renderDay($day_count, $count % $this->widthRenderedCalendarInDays);
+                $day_count++;
+            }
+
+            $count++;
+
+            if ($count % $this->widthRenderedCalendarInDays === 0) {
+                $this->renderRowEnd();
+                $this->renderRowStart();
+            }
+        }
+        $this->renderRowEnd();
+    }
+
+    private function renderEmptyDay()
+    {
+        echo "<td class='calendar_entry'></td>";
+    }
+
+    private function renderDay($monthDay, $weekDay)
+    {
+        echo("<td class='calendar_entry " . $this->calenderEntryWeekdayClasses[$weekDay] . "'>");
+        echo("<div class='entry_date'>$monthDay</div>");
+        echo("<div class='entry_line'></div>");
+        echo("<div class='entry_line'></div>");
+        echo("<div class='entry_line'></div>");
+        echo("</td>");
+    }
+}
