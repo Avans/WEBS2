aebff27e58e8a58ed1264dc64295a30633d450a6
Rik Meijer <ha.meijer@avans.nl>
2019-01-14 12:58:27 +0100

    use twig extension to yield PHPStorm support

--- Week 3/Twig-v1/index.php
+++ Week 3/Twig-v1/index.php
@@ -4,4 +4,4 @@ require_once __DIR__ . '/vendor/autoload.php';
 $loader = new Twig_Loader_Filesystem(__DIR__ . DIRECTORY_SEPARATOR . 'views');
 $twig = new Twig_Environment($loader);
 
-echo $twig->render('index.html', ['name' => 'World']);
+echo $twig->render('index.twig', ['name' => 'World']);
