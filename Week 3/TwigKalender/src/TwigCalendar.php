e92dea9e366d5bffce477e8e2b88b0f406cfff0c
Rik Meijer <ha.meijer@avans.nl>
2019-01-14 13:28:29 +0100

    provide Twigged Calendar example

---
+++ Week 3/TwigKalender/src/TwigCalendar.php
@@ -0,0 +1,25 @@
+<?php
+
+
+namespace Calendar;
+
+
+class TwigCalendar
+{
+
+    private $calendar;
+    private $month;
+
+    public function __construct(\Calendar\Calendar $calendar, \Calendar\Month $month)
+    {
+        $this->calendar = $calendar;
+        $this->month = $month;
+    }
+
+    public function render() {
+        for ($month_count = 1; $month_count <= 12; $month_count++) {
+            $this->calendar->renderMonth($this->month);
+            $this->month = $this->month->next();
+        }
+    }
+}
