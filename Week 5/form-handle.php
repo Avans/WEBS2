5004d754a4df86299f16519db333ef050b1c976e
Rik Meijer <ha.meijer@avans.nl>
2019-01-30 11:09:59 +0100

    initial examples Week 5

---
+++ Week 5/form-handle.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<head>
+    <title>voorbeeld.php</title>
+</head>
+<body>
+<?php
+echo "Uw naam is " . $_POST["naam"] . "<br>";
+echo "Uw adres is " . $_POST["adres"] . "<br>";
+echo "Uw woonplaats is " . $_POST ["woonplaats"] . "<br>";
+?>
+</body>
+</html>
---
+++ Week 5/form.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<head>
+    <title>formulier.html</title>
+</head>
+<body>
+<form action=”voorbeeld.php” method=”POST”>
+    Naam: <input type=”text” name=”naam”><br>
+    Adres: <input type=”text” name=”adres”><br>
+    Woonplaats: <input type=”text” name=”woonplaats”><br>
+    <input type=”submit” name=”verzenden” value=”Verzend!!”>
+</form>
+</body>
+</html>
