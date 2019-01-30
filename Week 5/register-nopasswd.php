0262999efc2d8a95811d3781f581e32bcd210364
Rik Meijer <ha.meijer@avans.nl>
2019-01-30 17:03:42 +0100

    write data to json file

--- Week 5/register-nopasswd.php
+++ Week 5/register-nopasswd.php
@@ -67,6 +67,7 @@ if (strcasecmp($_SERVER['REQUEST_METHOD'], 'get') === 0) {
     if (count($errors) > 0) {
         renderForm($errors);
     } else {
+        file_put_contents(sys_get_temp_dir() . DIRECTORY_SEPARATOR . $_POST['email'] . '.json', json_encode($_POST));
         ?>Dankjewel, <?=htmlentities($_POST['naam']);?> voor jouw registratie!<?php
     }
 
