cbe4a3f196701aeef209d7abcef48be8ab3d1c52
Rik Meijer <ha.meijer@avans.nl>
2019-02-12 14:34:13 +0100

    add new text example

--- Week 6/locale.php
+++ Week 6/locale.php
@@ -5,5 +5,6 @@ require __DIR__ . DIRECTORY_SEPARATOR . 'i18n_setup.php';
 
 echo '<h1>' . setlocale(5, '0') . '</h1>';
 
-echo _('Welcome');
+echo _('Welcome') . '<br>';
+echo _('This is an awesome translation');
 echo '<br>' . strftime("%A %d %B %Y", mktime(0, 0, 0, 12, 22, 1978));
